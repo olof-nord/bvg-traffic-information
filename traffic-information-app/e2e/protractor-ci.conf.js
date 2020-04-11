607a5b65cf09bd3d5d82ec1d942226e094cae0cc
Olof Nord <olof.nord@tutanota.com>
2020-04-11 12:31:18 +0200

    Manage the webdriver version manually to allow a downgrade from v81 to v80 as to stay compatible with the Puppeteer version of Chromium

--- traffic-information-app/e2e/protractor-ci.conf.js
+++ traffic-information-app/e2e/protractor-ci.conf.js
@@ -1,5 +1,8 @@
 const config = require('./protractor.conf').config;
 
+// For a full list of the Selenium arguments, see here:
+// https://peter.sh/experiments/chromium-command-line-switches/
+
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
