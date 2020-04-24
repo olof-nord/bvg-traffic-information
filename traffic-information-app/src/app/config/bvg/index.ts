2ac7c0440b9137bba0660c1ae468b5148bf6beb6
Olof Nord <olof.nord@tutanota.com>
2020-04-24 21:26:47 +0200

    Adding names of all M1 - M10 tram stops

--- traffic-information-app/src/app/config/bvg/index.ts
+++ traffic-information-app/src/app/config/bvg/index.ts
@@ -2,6 +2,7 @@ import { busLines, busColor } from './bus-lines';
 import { ferryLines, ferryColor } from './ferry-lines';
 import { ferryStops } from './ferry-stops';
 import { tramLines, tramColor } from './tram-lines';
+import { tramStops } from './tram-stops';
 import { undergroundLines } from './underground-lines';
 import { undergroundStations } from './underground-stations';
 
@@ -13,6 +14,7 @@ export {
   ferryStops,
   tramLines,
   tramColor,
+  tramStops,
   undergroundLines,
   undergroundStations
 };
